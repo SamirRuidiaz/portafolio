5f0db3d425b5a5927c1465524ad7b47d66d18fad
Samir Ruidiaz <samirruidiaz@gmail.com>
2021-01-26 23:15:11 -0500

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,24 +9,20 @@ export const Footer = () => {
   return(
     <footer className={`${styles.Footer} py-5`}>
       <div className="container">
-        {/* <p className="float-end mb-1">
-          <button>Back to top</button>
-        </p> */}
-
         <section id="socials-4" className="widget widget_socials">
-          <div className="socials_widget_wrapper socials">
-            <ul className={styles.Footer__ul_socials}>
-              <li>
+
+            <div className={styles.Footer__socials}>
+              <span>
                 <i className="fab fa-facebook"></i>
-              </li>
-              <li>
+              </span>
+              <span>
                 <i className="fab fa-instagram"></i>
-              </li>
-              <li>
+              </span>
+              <span>
                 <i className="fab fa-linkedin"></i>
-              </li>
-            </ul>
-          </div>
+              </span>
+            </div>
+
         </section>
         <p className="mb-1">&copy; Todo los derechos reservados. Developed by <b>{owner}.</b></p>
       </div>
