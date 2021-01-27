33b814270ad59018728764f19704524d63064dfd
Samir Ruidiaz <samirruidiaz@gmail.com>
2021-01-26 22:45:00 -0500

    update path img

--- src/data/data.js
+++ src/data/data.js
@@ -3,60 +3,60 @@ export const data = [
     name: 'Qrvey',
     descrip: 'Este enfoque innovador de la analítica cambió para siempre el espacio de la analítica integrada y redefinió la forma en que las empresas de software distribuyen información y conocimientos a sus usuarios y partes interesadas. Usando sus décadas de experiencia combinada, muchos en ese equipo original se han reunido nuevamente para el próximo salto tecnológico con Qrvey. Qrvey se fundó en 2016 para marcar el comienzo de una nueva generación de análisis de negocios nativos de la nube en AWS.', 
     url: 'https://qrvey.com/',
-    img: './portafolio/qrvey.png'
+    img: './qrvey.png'
   },
   {
     name: 'xpense',
     descrip: 'Xpense. Solución para el control de gastos empresariales. Mantenga el control, Transparencia en las legalizaciones disminuyendo así el riesgo de fraude, Visibilidad del cumplimiento de políticas de gastos en su compañía. Mejore la productividad, el cumplimiento y el control de los gastos de su empresa.', 
     url: 'https://www.xpense.co',
-    img: './portafolio/escritorio-xpe.png'
+    img: './escritorio-xpe.png'
   },
   {
     name: 'sutramiteenlinea',
     descrip: 'Plataforma para el procesamiento de todo tipo de trámites en línea de manera segura. Ayude a sus usuarios finales a procesar trámites de manera virtual y desde cualquier lugar sin necesidad de desplazarse y/o hacer colas. Desde trámites sencillos hasta pagos en línea.', 
     url: 'https://www.sutramiteenlinea.co/',
-    img: './portafolio/sutramiteenlinea.png'
+    img: './sutramiteenlinea.png'
   },
   {
     name: 'Nativapps',
     descrip: 'Reclutamos y asignamos cuidadosamente los recursos de TI de acuerdo con los roles que necesita su equipo. Creamos equipos de soporte y de extremo a extremo, asignando los roles correctos y la dedicación a su producto. Somos expertos en el medio ambiente local con experiencia en la construcción y ejecución de operaciones de marca blanca sin voz en Colombia.', 
     url: 'https://www.nativapps.com/',
-    img: './portafolio/nativapps.png'
+    img: './nativapps.png'
   },
   {
     name: 'Biofood',
     descrip: 'Biofood es una solución para la gestión de cafeterías escolares que ayuda a los colegios y padres a controlar y promover hábitos saludables en la nutrición de los estudiantes y miembros del staff, mejora los procesos internos y reduce el desperdicio de comida.', 
     url: 'http://biofoodsoftware.com/',
-    img: './portafolio/Dashboard-beneficios.png'
+    img: './Dashboard-beneficios.png'
   },
   {
     name: 'PNG',
     descrip: 'Construimos y desarrollamos software personalizado para cubrir las necesidades de tu empresa. Diseñar productos interactivos más simples, mejorando la interacción del usuario con el dispositivo.', 
     url: 'http://www.pngweb.co/',
-    img: './portafolio/Mockups-PC-2.png'
+    img: './Mockups-PC-2.png'
   },
   {
     name: 'Meico',
     descrip: 'Somos una empresa líder en ventas en cada uno de los mercados en donde participamos, comprometidos con la excelencia en el servicio al cliente y cumpliendo con las expectativas de rentabilidad de los accionistas.', 
     url: 'http://www.meico.com.co/',
-    img: './portafolio/meico.jpg'
+    img: './meico.jpg'
   },
   {
     name: 'Sumind',
     descrip: 'A través de la tecnología e innovación, optimizamos los procesos de compra, entrega e inventario de insumos industriales para la compañía Sumind. (Mi Sumind) Plataforma de autogestión comercial. (Biostock) Software para control de entregas y manejo de inventarios de insumos industriales.', 
     url: 'https://sumind.com.co/',
-    img: './portafolio/Sumind-plataforma.png'
+    img: './Sumind-plataforma.png'
   },
   {
     name: 'Smadia Clinic',
     descrip: 'Somos apasionados y creativos, con una necesidad constante de innovación e investigación. Nuestra prioridad son las personas, darle forma a sus deseos y lograr que se sientan más seguros de si mismos. Smadia Clinic, pone a tu disposición todo el talento y sensibilidad humana, junto a la tecnología más avanzada, para realizar de manera segura y confiable el procedimiento más apropiado para tu necesidad.', 
     url: 'https://www.smadiaclinic.com/index/smadiaclinic',
-    img: './portafolio/smadiaclinic.jpg'
+    img: './smadiaclinic.jpg'
   },
   {
     name: 'Oficaribe',
     descrip: 'Hablamos de PASIÓN porque sabemos lo que esa palabra significa. Fue la pasión la que nos hizo creer, hace más de 20 años, que un pequeño negocio familiar podría volverse protagonista de toda una industria. La que nos llevó a despertar cada mañana más temprano e irnos a la cama más tarde pensando en nuevas y mejores maneras de brindarle a nuestros clientes, más que mobiliarios, soluciones.', 
     url: 'https://www.oficaribe.com/',
-    img: './portafolio/oficaribe.png'
+    img: './oficaribe.png'
   }
 ]
